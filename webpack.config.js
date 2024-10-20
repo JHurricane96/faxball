6e18866b6c125de70076163e34d246ef8135b393
Arun Ramachandran <arun.jhurricane@gmail.com>
2024-10-20 18:00:02 -0400

    Add public path to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: process.env.NODE_ENV === 'production' ? '/faxball/' : '/',
   },
   devServer: {
     static: './',
